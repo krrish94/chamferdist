41c1467d3a3e4206b736e041832da4985874d485
Krishna Murthy <krrish94@gmail.com>
2019-09-03 00:18:57 -0400

    Add Cython to requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
+long_description = 'A pytorch module to compute Chamfer distance \
+                    between two point sets (pointclouds).'
+
+requirements = [
+    'Cython',
+    'torch>1.1.0',
+]
+
 setup(
     name='chamferdist',
+    version='0.2.0',
+    description='Pytorch Chamfer distance', 
+    long_description=long_description,
     ext_modules=[
         CUDAExtension('chamferdistcuda', [
             'chamferdist/chamfer_cuda.cpp',
