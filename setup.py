79e3f79c8f3445f84e39b275bba7348b2339f73e
Krishna Murthy <krrish94@gmail.com>
2019-09-03 00:19:06 -0400

    Add Cython to requirements

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     version='0.2.0',
     description='Pytorch Chamfer distance', 
     long_description=long_description,
+    requirements=requirements,
     ext_modules=[
         CUDAExtension('chamferdistcuda', [
             'chamferdist/chamfer_cuda.cpp',
